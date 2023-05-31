b2ccadbecfc55a67c438a211a9ac9db56b9f4fc6
Serious Zainab <85008588+zainab-oss@users.noreply.github.com>
2023-05-31 13:00:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-img = "";
+want = "";
 status = "";
 objects = [];
 
 function setup()
 {
-    canvas = createCanvas(380, 380);
+    canvas = createCanvas(380, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(380,380);
+    video.size(380,300);
     video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    want = document.getElementById("object").value;
+}
+
+function modelLoaded()
+{
+    console.log("ModelLoaded");
+    status = true;
 }
 
 function draw()
 {
-    image(video, 0, 0, 380, 380);
+    image(video, 0, 0, 380, 300);
 }
